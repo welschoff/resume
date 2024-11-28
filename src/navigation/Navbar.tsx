418cfb0be26646837b33dac3d5275d3dabce1881
welschoff <h.welschoff@gmx.de>
2024-11-28 18:01:55 +0100

    🎨 Improve code structure

--- src/navigation/Navbar.tsx
+++ src/navigation/Navbar.tsx
@@ -1,41 +1,34 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, To } from 'react-router-dom';
 import styles from './Navbar.module.scss';
+import { ReactNode } from 'react';
+
+interface NavbarLinkProps {
+  to: To;
+  children: ReactNode;
+}
+
+function NavbarLink({ to, children }: NavbarLinkProps) {
+  return (
+    <NavLink
+      to={to}
+      style={({ isActive }) => ({
+        textDecoration: isActive ? 'underline' : '',
+        color: isActive ? '#282828' : '',
+      })}
+    >
+      {children}
+    </NavLink>
+  );
+}
 
 function Navbar() {
   return (
     <header className={styles.navbar}>
       <h1>Hendrik Welschoff</h1>
       <nav className={styles.navLinks}>
-        <NavLink
-          to="/"
-          style={({ isActive }) => {
-            return {
-              textDecoration: isActive ? 'underline' : '',
-            };
-          }}
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/skills"
-          style={({ isActive }) => {
-            return {
-              textDecoration: isActive ? 'underline' : '',
-            };
-          }}
-        >
-          Skills
-        </NavLink>
-        <NavLink
-          to="/experience"
-          style={({ isActive }) => {
-            return {
-              textDecoration: isActive ? 'underline' : '',
-            };
-          }}
-        >
-          Experience
-        </NavLink>
+        <NavbarLink to="/">Home</NavbarLink>
+        <NavbarLink to="/skills">Skills</NavbarLink>
+        <NavbarLink to="/experience">Experience</NavbarLink>
       </nav>
     </header>
   );
