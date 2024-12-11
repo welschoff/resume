b56db57dc6ae054fc45b5481666c20e08b3571f2
welschoff <h.welschoff@gmx.de>
2024-12-11 10:59:49 +0100

    ✨ Add skills

--- src/pages/skills/Skills.tsx
+++ src/pages/skills/Skills.tsx
@@ -10,10 +10,13 @@ import jest from '../../assets/jest.png';
 import tailwind from '../../assets/tailwind.png';
 import bootstrap from '../../assets/bootstrap.png';
 import next from '../../assets/next.png';
+import testcafe from '../../assets/testcafe.png';
+import readyapi from '../../assets/readyapi.png';
 import styles from './Skills.module.scss';
+import postman from '../../assets/postman.png';
+import cypress from '../../assets/cypress.png';
 import SkillComponent from '../../components/SkillComponent/SkillComponent';
 import CategoryComponent from '../../components/CategoryComponent/CategoryComponent';
-import { className } from '../../../node_modules/@sinonjs/commons/types/index.d';
 
 function Skills() {
   return (
@@ -21,22 +24,21 @@ function Skills() {
       <div className={styles.col1}>
         <CategoryComponent
           title="Markup Language"
-          skill={<SkillComponent logo={html} name="HTML5" rating="20%" />}
+          skill={<SkillComponent logo={html} name="HTML5" rating="90%" />}
         />
         <CategoryComponent
-          title="Frameworks & Libraries"
+          title="Frontend Frameworks & Libraries"
           skill={
             <>
               <SkillComponent logo={react} name="React" rating="80%" />
               <SkillComponent logo={angular} name="Angular" rating="75%" />
-              <SkillComponent logo={next} name="Next.js" rating="90%" />
-              <SkillComponent logo={jest} name="Jest" rating="90%" />
+              <SkillComponent logo={next} name="Next.js" rating="60%" />
               <SkillComponent
                 logo={tailwind}
                 name="Tailwind CSS"
-                rating="90%"
+                rating="60%"
               />
-              <SkillComponent logo={bootstrap} name="Bootstrap" rating="90%" />
+              <SkillComponent logo={bootstrap} name="Bootstrap" rating="70%" />
             </>
           }
         />
@@ -49,9 +51,9 @@ function Skills() {
               <SkillComponent
                 logo={typescript}
                 name="Typescript"
-                rating="75%"
+                rating="80%"
               />
-              <SkillComponent logo={js} name="JavaScript" rating="75%" />
+              <SkillComponent logo={js} name="JavaScript" rating="80%" />
             </>
           }
         />
@@ -65,13 +67,32 @@ function Skills() {
             </>
           }
         />
+        <CategoryComponent
+          title="API Tools"
+          skill={
+            <>
+              <SkillComponent logo={readyapi} name="ReadyAPI" rating="80%" />
+              <SkillComponent logo={postman} name="Postman" rating="80%" />
+            </>
+          }
+        />
       </div>
       <div className={styles.col3}>
         <CategoryComponent
           title="Database"
           skill={
             <>
-              <SkillComponent logo={mongodb} name="MongoDB" rating="90%" />
+              <SkillComponent logo={mongodb} name="MongoDB" rating="75%" />
+            </>
+          }
+        />
+        <CategoryComponent
+          title="Testing Frameworks"
+          skill={
+            <>
+              <SkillComponent logo={jest} name="Jest" rating="50%" />
+              <SkillComponent logo={cypress} name="Cypress" rating="55%" />
+              <SkillComponent logo={testcafe} name="Testcafe" rating="60%" />
             </>
           }
         />
