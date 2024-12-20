8bcca7ed23a087f4471e1f9e7f999e4bf2834ed9
welschoff <h.welschoff@gmx.de>
2024-12-20 13:15:01 +0100

    ✨ Create Experience Component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './navigation/Navbar';
 import Home from './pages/home/Home';
 import Skills from './pages/skills/Skills';
+import Experience from './pages/experience/Experience';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/skills" element={<Skills />} />
+        <Route path="/experience" element={<Experience />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/experience/Experience.tsx
@@ -0,0 +1,7 @@
+import styles from './Experience.module.scss';
+
+function Experience() {
+  return <div className={styles.experienceContainer}>Experience</div>;
+}
+
+export default Experience;
