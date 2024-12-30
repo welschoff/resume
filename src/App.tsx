2458dc661d6511faad68dac8b06682d43e36c35b
welschoff <h.welschoff@gmx.de>
2024-12-30 17:02:09 +0100

    Declare Router as HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './navigation/Navbar';
 import Home from './pages/home/Home';
 import Skills from './pages/skills/Skills';
