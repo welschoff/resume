aeb179294b92975c0277c2e87d9520b70c09e1a1
welschoff <h.welschoff@gmx.de>
2025-02-07 15:01:40 +0100

    ✨ Add logo to Navbar

--- src/navigation/Navbar.tsx
+++ src/navigation/Navbar.tsx
@@ -23,7 +23,24 @@ function NavbarLink({ to, children }: NavbarLinkProps) {
 function Navbar() {
   return (
     <header className={styles.navbar}>
-      <span className={styles.logo}>HW</span>
+      <div className={styles.logoContainer}>
+        <div className={styles.svgContainer}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            id="Layer_1"
+            data-name="Layer 1"
+            viewBox="0 0 24 24"
+            width="40"
+            height="40"
+          >
+            <path
+              d="m12,0C5.383,0,0,5.383,0,12s5.383,12,12,12,12-5.383,12-12S18.617,0,12,0Zm5,17.75h-2v-4.75h-6v4.75h-2V6.25h2v4.75h6v-4.75h2v11.5Z"
+              fill="var(--secondary-color)"
+            />
+          </svg>
+        </div>
+        <span>Hendrik Welschoff</span>
+      </div>
       <nav className={styles.navLinks}>
         <NavbarLink to="/">Home</NavbarLink>
         <NavbarLink to="/skills">Skills</NavbarLink>
