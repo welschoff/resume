16eba4846624c6ddcc7475d2a284671ce1c3e5ce
Hendrik <h.welschoff@gmx.de>
2026-08-04 17:33:58 +0200

    Remove page transition

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,7 +1,4 @@
 import { Routes, Route } from 'react-router-dom';
-
-import { TransitionProvider } from '../context/TransitionContext';
-import TransitionComponent from '../components/Transition/Transition';
 import Home from '../pages/home/Home';
 import Resume from '../pages/resume/Resume';
 import Contact from '../pages/contact/Contact';
@@ -9,42 +6,12 @@ import Skills from '../pages/skills/Skills';
 
 const Router = () => {
   return (
-    <TransitionProvider>
-      <Routes>
-        <Route
-          index
-          element={
-            <TransitionComponent>
-              <Home />
-            </TransitionComponent>
-          }
-        />
-        <Route
-          path="/skills"
-          element={
-            <TransitionComponent>
-              <Skills />
-            </TransitionComponent>
-          }
-        />
-        <Route
-          path="/resume"
-          element={
-            <TransitionComponent>
-              <Resume />
-            </TransitionComponent>
-          }
-        />
-        <Route
-          path="/contact"
-          element={
-            <TransitionComponent>
-              <Contact />
-            </TransitionComponent>
-          }
-        />
-      </Routes>
-    </TransitionProvider>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="/skills" element={<Skills />} />
+      <Route path="/resume" element={<Resume />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
   );
 };
 
