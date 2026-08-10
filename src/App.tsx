407ee6491652209c78fb6a44f40f2de02246ff28
Hendrik <h.welschoff@gmx.de>
2026-08-10 17:49:25 +0200

    Remove router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
 import './index.css';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
-
 import Navbar from './navigation/Navbar';
-import Router from './router/Router';
+import Home from './pages/home/Home';
+import Skills from './pages/skills/Skills';
+import Resume from './pages/resume/Resume';
+import Contact from './pages/contact/Contact';
 
 gsap.registerPlugin(useGSAP);
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Router />
-    </>
+    <div>
+      <header>
+        <Navbar />
+      </header>
+
+      <main>
+        <section id="home">
+          <Home />
+        </section>
+
+        <section id="skills">
+          <Skills />
+        </section>
+
+        <section id="resume">
+          <Resume />
+        </section>
+
+        <section id="contact">
+          <Contact />
+        </section>
+      </main>
+    </div>
   );
 }
 
